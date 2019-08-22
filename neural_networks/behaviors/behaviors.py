a30d068d1b378e447b1ba1deded616a6c3aff9bf
xueli-zhang <xueli.zhang@carleton.ca>
2019-08-21 20:38:18 -0400

    add behavior class

---
+++ neural_networks/behaviors/behaviors.py
@@ -0,0 +1,8 @@
+#This class is to create behavior class, which includes action, speed, angle, etc...
+
+class Behavior:
+
+    def __init__(self, action, speed, angle):
+        self.action = action
+        self.speed = speed
+        self.angle = angle
